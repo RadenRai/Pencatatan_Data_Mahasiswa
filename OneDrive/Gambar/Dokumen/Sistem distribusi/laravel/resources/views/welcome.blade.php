f6f0ad31fb16a95b011452e447fb75342aeea5a5
raihan ramadhan <radenraihanramadhan@gmail.com>
2024-02-15 20:33:14 +0700

    first commit

---
+++ OneDrive/Gambar/Dokumen/Sistem distribusi/laravel/resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Home</title>
+</head>
+<body>
+  <h1>Tolong login</h1>
+  <p></p>
+  <p>
+  <img src="hehehe.jpeg" alt="Image" height="300" width="300">
+  </p>
+  <h1>Ayo coblos paslon no 3 </h1>
+</body>
+</html>
